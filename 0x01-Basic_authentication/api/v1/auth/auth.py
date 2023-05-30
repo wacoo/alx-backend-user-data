d4a7c601b4a8c079d570989cdf28c0fe02b26e59
wac <wabaham9@gmail.com>
2023-05-30 17:50:48 +0200

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,6 +8,13 @@ class Auth:
     ''' Auth class that manages API authentication '''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         ''' requires authentication '''
+        path_s = path
+        if path is not None and path[-1] != '/':
+            path_s = path + '/'
+        if not path or excluded_paths is None or len(excluded_paths) == 0\
+                or (path not in excluded_paths and
+                    path_s not in excluded_paths):
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
