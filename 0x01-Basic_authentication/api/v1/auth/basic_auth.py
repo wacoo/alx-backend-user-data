08c85f52b3473dd7f280f79e1ff5893c7623cf66
wac <wabaham9@gmail.com>
2023-05-30 23:18:39 +0300

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,10 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     ''' BasicAuth class '''
+    def extract_base64_authorization_header(self, authorization_header: str
+                                            ) -> str:
+        ''' ''decode credentials '''
+        ah = authorization_header
+        if ah is None or type(ah) != str or ah[:6] != 'Basic ':
+            return None
+        return ah[6:]
