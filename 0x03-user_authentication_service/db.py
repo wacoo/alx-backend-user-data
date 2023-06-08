c5b51a2e16fdae0b853595818626a44644cc178b
wac <wabaham9@gmail.com>
2023-06-08 15:51:43 +0300

    task 2 correction

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 """DB module
 """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select, tuple_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base
 from typing import TypeVar
 from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 class DB:
@@ -41,4 +43,20 @@ class DB:
         except Exception:
             self._session.rollback()
             return None
+        return user
+
+    def find_user_by(self, **kwargs):
+        ''' return the first user data based on input '''
+        ukey = []
+        uval = []
+        for key, val in kwargs.items():
+            if hasattr(User, key):
+                ukey.append(getattr(User, key))
+                uval.append(val)
+            else:
+                raise InvalidRequestError
+        user = self._session.query(User).filter(
+                tuple_(*ukey).in_([tuple(uval)])).first()
+        if user is None:
+            raise NoResultFound()
         return user
