bae052be12beffae5f5213132ef2db054749927d
wac <wabaham9@gmail.com>
2023-06-08 21:04:48 +0300

    task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,6 +52,16 @@ class Auth:
             return result
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        ''' returns new session id '''
+        try:
+            user = self._db.find_user_by(email=email)
+            s_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=s_id)
+            return s_id
+        except NoResultFound:
+            return None
     '''
             if user is None:
             hashed = _hash_password(password)
