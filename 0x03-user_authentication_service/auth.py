6d0f9b980c163d0430ef507a996c737c12a9351c
wac <wabaham9@gmail.com>
2023-06-08 22:21:30 +0300

    task 13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -74,3 +74,9 @@ class Auth:
         except NoResultFound:
             return None
         return user
+
+    def destroy_session(self, user_id: int) -> None:
+        ''' destroys session '''
+        if user_id is None:
+            return None
+        self._db.update_user(user_id, session_id=None)
